e5b9117cbb1ec469cd3490655e23aa434f5f8f2d
JOlsonMam <jessiebess02@aol.com>
2025-07-10 10:25:16 -0500

    Update streamlit_app.py
    added spaces between equal sign in cnx variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie :cup_with_straw:")
@@ -9,24 +9,13 @@ st.write(
   """
 )
 
-import streamlit as st
-
 name_on_order = st.text_input("Name on smoothie:")
 st.write("The name on the smoothie will be:", name_on_order)
-from snowflake.snowpark.functions import col
-
-#option = st.selectbox(
-#    "What is your favorite fruit?",
-#    ("Bananas", "Strawberries", "Peaches"),
-#)
-
-#st.write("Your favorite fruit is:", option)
 
 #session = get_active_session()
-cnx=st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
@@ -42,22 +31,13 @@ if ingredients_list:
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
     
-    #st.write(ingredients_string)
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
-    
+  
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
-        
-    #st.write(my_insert_stmt)
 
     #if ingredients_string:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-
-#st.write("You selected:", options)
